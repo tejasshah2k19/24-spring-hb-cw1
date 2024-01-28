c2e67263e39665538f98955a7969bd6b6e88ee96
Tejas Shah <tejasshah2k19@gmail.com>
2024-01-28 12:33:47 +0530

    first

---
+++ src/main/java/com/entity/ProductEntity.java
@@ -0,0 +1,61 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer productId;
+	String name;
+	Float price;
+	Integer qty;
+	String categoryName;
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public Integer getQty() {
+		return qty;
+	}
+
+	public void setQty(Integer qty) {
+		this.qty = qty;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+}
